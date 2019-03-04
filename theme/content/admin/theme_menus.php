ce1f1c36f5fe904b00fdbe83a037e91203ffb2d7
Frank Luongo <frankluongo92@gmail.com>
2019-03-03 19:31:12 -0500

    Removing extra menus

--- theme/content/admin/theme_menus.php
+++ theme/content/admin/theme_menus.php
@@ -1,9 +1,7 @@
 <?php
 function register_menu() {
-    register_nav_menus(array( // Using array to specify more menus if needed
-        'header-menu' => __('Header Menu', 'main-nav'), // Main Navigation
-        'sidebar-menu' => __('Sidebar Menu', 'sidebar-nav'), // Sidebar Navigation
-        'extra-menu' => __('Extra Menu', 'extra-nav'), // Extra Navigation if needed (duplicate as many as you need!)
-        'utility-nav' => __('Utility Nav', 'utility-nav') // Extra Navigation if needed (duplicate as many as you need!)
+    register_nav_menus(array(
+        'header-menu' => __('Header Menu', 'main-nav'),
+        'sidebar-menu' => __('Sidebar Menu', 'sidebar-nav')
     ));
 }
